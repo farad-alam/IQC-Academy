48e10d1d4c4af44016dffdf25e9fdb818fa2b7f3
Farad Alam <farad.dev@gmail.com>
2026-06-23 12:19:32 +0600

    UI: Split footer top section into 3 columns (Contact, Socials, Maps)

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -7,16 +7,39 @@ export default function Footer() {
     <footer className={styles.footer}>
       {/* Left Panel */}
       <div className={styles.leftPanel}>
-        <div className={styles.contactSection}>
-          <h3 className={styles.contactTitle}>যোগাযোগ</h3>
-          <ul className={styles.contactList}>
-            <li>ইমেইল: info@iqcacademy.com</li>
-            <li>মোবাইল: +880 1234 567890</li>
-            <li>ঠিকানা: ঢাকা, বাংলাদেশ</li>
-          </ul>
-          <div className={styles.socials}>
-            <a href="#" aria-label="Facebook">FB</a>
-            <a href="#" aria-label="YouTube">YT</a>
+        <div className={styles.topGrid}>
+          {/* Column 1: Contact */}
+          <div className={styles.gridItem}>
+            <h3 className={styles.contactTitle}>যোগাযোগ</h3>
+            <ul className={styles.contactList}>
+              <li>ইমেইল: info@iqcacademy.com</li>
+              <li>মোবাইল: +880 1234 567890</li>
+              <li>ঠিকানা: ঢাকা, বাংলাদেশ</li>
+            </ul>
+          </div>
+
+          {/* Column 2: Social Media */}
+          <div className={styles.gridItem}>
+            <h3 className={styles.contactTitle}>সোশ্যাল মিডিয়া</h3>
+            <div className={styles.socialsGrid}>
+              <a href="#" aria-label="Facebook">FB</a>
+              <a href="#" aria-label="YouTube">YT</a>
+              <a href="#" aria-label="Instagram">IG</a>
+              <a href="#" aria-label="Twitter">X</a>
+            </div>
+          </div>
+
+          {/* Column 3: Google Maps */}
+          <div className={`${styles.gridItem} ${styles.mapContainer}`}>
+            <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14608.0369448503!2d90.3671072!3d23.74705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b33cffc3fb%3A0x4a826f475fd312af!2sDhanmondi%2C%20Dhaka%201205!5e0!3m2!1sen!2sbd!4v1717600000000!5m2!1sen!2sbd" 
+              width="100%" 
+              height="100%" 
+              style={{ border: 0 }} 
+              allowFullScreen="" 
+              loading="lazy" 
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
           </div>
         </div>
 
